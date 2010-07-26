dc99ea704183e457b9db3ae74c7b3f60a2e954ed
Brent Kirby <brent@kurbmedia.com>
2010-07-26 14:48:19 -0400

    Added can! methods to assign permission to a resource or resources

--- lib/permissable/member.rb
+++ lib/permissable/member.rb
@@ -18,14 +18,40 @@ module Permissable
       # Alias to can? to get the inverse.
       def cannot?(method, resource); !can?(method, resource); end
       
+      # Assign permissions to a member if they don't already exist.
+      # TODO: There has to be a friendlier way to mass assign permissions to roles rather than
+      # looping each one for existence and then saving separately.
+      def can!(methods, resources)
+        [resources].flatten.each do |resource|
+          
+          # Kind of unecessary but since some methods allow you to specify a Classname directly, this just
+          # safegaurds against trying to do the same here.
+          next if resource.is_a?(Class)
+          
+          # Get the member identifier for this resource
+          identifier = member_identifier(resource)
+          
+          [methods].flatten.each do |method|
+            # Permission already exists, continue.
+            next if can?(method, resource)
+            
+            # Create a new permission for each member (once if its self, multiple times if its associated)
+            [identifier[:member_id]].flatten.each do |member_id|
+              perm = Permission.new(:member_id => member_id, :member_type => identifier[:member_type], :permission_type => method.to_s.downcase)
+              perm.resource = resource
+              perm.save
+            end
+          end
+        end
+        
+      end
       
       # This sets the member information for our permission lookup based on the current resource scope.
       # These attributes correspond to the correct member_id and member_type in our permissions table.
-      def member_identifier(scope)
+      def member_identifier(resource)
         
         @member_identifier ||= {}
-        # The scope should be the classname of a resource we are getting identifiers for
-        scope = scope.to_s.classify
+        scope = fetch_scope(resource)
         
         return @member_identifier[scope] unless @member_identifier[scope].nil?
         return { :member_id => self.id, :member_type => self.class.to_s } unless permissable_associations.has_key?(scope)
@@ -44,11 +70,23 @@ module Permissable
       
       # Looks up permissions for a particular resource.
       def permissions_for(resource, methods = nil)
-        scope  = resource.class.to_s.classify
-        return self.permissions unless permissable_associations.has_key?(scope)
+        scope  = fetch_scope(resource)
+        return self.permissions.with_permission_to(methods) unless permissable_associations.has_key?(scope)
         relation = Permission.where(member_identifier(scope)).for_resource(resource)
         relation = relation.with_permission_to(methods) unless methods.nil?
         relation
+      end
+      
+      # Returns the member responsible for this resource. This can either be an instance of self, or an instance or 
+      # array of assocated instances.
+      def permissable_member(resource)
+        scope  = fetch_scope(resource)
+        (permissable_associations.has_key?(scope)) ? send("#{permissable_associations[scope]}".to_sym) : self
+      end
+      
+      def fetch_scope(resource)
+        return resource if resource.is_a?(String)
+        (resource.is_a?(Class)) ? resource.to_s : resource.class.to_s.classify
       end
       
     end
